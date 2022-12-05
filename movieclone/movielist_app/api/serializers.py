5ec178772fad5816202b440c407d5914a3020844
Nirupamaa Jaya <nirupamaajaya@parthasarathys-MacBook-Pro.local>
2022-12-05 13:58:01 +0100

    serializer and api view

---
+++ movieclone/movielist_app/api/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from movielist_app.models import Movie
+
+class MovieSerializer(serializers.Serializer):
+    # class Meta:
+    #     model = Movie
+    #     fields = '__all__'
+        
+    # def validate(self, data):
+    #     if data['name'] == data['description']:
+    #         raise serializers.ValidationError('Title and Description should be different!')
+    #     else:
+    #         return data
+    
+    # def validate_name(self, value):
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError('Name is too short !!s')
+        
+    id = serializers.IntegerField(read_only=True)
+    name= serializers.CharField()
+    description = serializers.CharField()
+    active = serializers.BooleanField()
+    
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
